63851a4b9f9495f53398322ae54272d3062756a4
Vinicius <marcusvmdiass@gmail.com>
2026-08-19 23:04:33 -0400

    Adicionar exercicio6 de media

---
+++ exercicio6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () {
+    float n1, n2, media;
+
+    printf("Digite a sua primeira nota: ");
+    scanf("%f", &n1);
+
+    printf("Digite a sua segunda nota: ");cd .\C-estudos
+    scanf("%f", &n2);
+
+    media = (n1 + n2) / 2;
+    printf("A media eh: %.2f\n", media);
+
+    return 0;
+    
+}
