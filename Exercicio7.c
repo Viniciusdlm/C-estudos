9d0e097fdd133945bdcaa7970175aa0aecc212fa
Vinicius <marcusmvdiass@gmail.com>
2026-08-20 23:06:28 -0400

    Exercicio de Leitura e Escrita de Entrada e Saida

---
+++ Exercicio7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("***********************\n");
+    printf("* BEM VINDO AO JOGO DA ADIVINHACAO *\n");
+    printf("***********************\n");
+
+    int numerosecreto = 42;
+
+    int chute;
+    printf("Qual e o seu chute? ");
+    scanf("%d", &chute);
+
+    printf("Voce chutou o numero %d\n", chute);
+
+    return 0;
+}
