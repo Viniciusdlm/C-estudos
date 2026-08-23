77a3301b0f5c6165c7a762b803a679b8b99829ae
Vinicius <marcusmvdiass@gmail.com>
2026-08-22 23:52:51 -0400

    Exercicio sobre os comentarios

---
+++ exerciciocomentarios.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// BARRA DUPLA SIGNIFICA UM COMENTARIO POR LINHA
+int main
+    printf("Olá mundo") // usando a barra dupla para adicionar um comentario em uma linha
+
+/*
+BARRA E ASTERICOS DA PARA FAZER UM COMENTARIO EM VARIAS LINHAS, INSTRUÇÃO EM BLOCOS.
+1. ABRIR O VS CODE
+2. ABRIR A PASTA DO CODIGO
+3. ETC..
+*/
