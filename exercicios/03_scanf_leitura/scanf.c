550796be050b48051a5ed06e825b456f2835a74a
Vinicius <marcusmvdiass@gmail.com>
2026-08-23 19:34:00 -0400

    Adiciona exercício de leitura com scanf

---
+++ exercicios/03_scanf_leitura/scanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int idade;                                           // guardando uma variavel
+    printf("\n\n--------DIGITE A SUA IDADE-------\n\n"); // peço para digitarem a idade
+    scanf("%d", &idade);                                 // espero o retorno do teclado referente a idade
+    printf("\n\tIdade: %d\n", idade);                    // mostro a idade na tela usando o scanf
+    
+}
