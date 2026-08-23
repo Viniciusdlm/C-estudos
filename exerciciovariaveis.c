2ef7439744e94f56df066315a608deb45262f4ba
Vinicius <marcusmvdiass@gmail.com>
2026-08-23 18:12:26 -0400

    Adicionado a operação de subtração

--- exerciciovariaveis.c
+++ exerciciovariaveis.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-int main (){
+int main()
+{
+    // %d serve para um numero decimal - inteiro
 
-    printf("%d + %d = %d",12,18,12+18);  // só dara certo se recompilar sempre que alterar o codigo
-    
+    printf("\n\t%d + %d = %d\n", 12, 18, 12 + 18); // só dara certo se recompilar sempre que alterar o codigo, esse codigo é para somas.
 
+    printf("\n\t%d - %d = %d\n", 15, 10, 15 - 10); // código para subtrair os numeros.
 
     return 0;
 }
