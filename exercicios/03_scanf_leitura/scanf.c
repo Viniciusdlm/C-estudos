ff55a79f54ad1bf644587ff1073dba3bac3f6599
Vinicius <marcusmvdiass@gmail.com>
2026-08-23 19:44:01 -0400

    Adicionado a leitura de dois valores com soma

--- exercicios/03_scanf_leitura/scanf.c
+++ exercicios/03_scanf_leitura/scanf.c
@@ -7,5 +7,14 @@ int main()
     printf("\n\n--------DIGITE A SUA IDADE-------\n\n"); // peço para digitarem a idade
     scanf("%d", &idade);                                 // espero o retorno do teclado referente a idade
     printf("\n\tIdade: %d\n", idade);                    // mostro a idade na tela usando o scanf
-    
+
+    int num1, num2;                                      // guardando variavel 
+    printf("\nDigite dois valores inteiros:\n");         // peco para digitar os dois valores
+    scanf("%d", &num1);                                  // le o valor1
+    scanf("%d", &num2);                                 // le o valor2
+    printf("\n\tSoma: %d\n", num1 + num2);              // realiza a leitura e soma dos dois valores
+
+
+
+
 }
