d598cdb00bfbfc8898d1c7df78c5a590c840bccd
Vinicius <marcusmvdiass@gmail.com>
2026-08-23 20:21:21 -0400

    Usando scanf para leitura em char

---
+++ exercicios/03_scanf_leitura/scanf_char.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    char sexo;
+
+    printf("\n\tDigite F para Feminino ou M para Masculino: ");
+    scanf("%c", & sexo);
+    printf("\n\tSexo Digitado: %c\n", sexo);
+
+
+
+
+
+
+}
