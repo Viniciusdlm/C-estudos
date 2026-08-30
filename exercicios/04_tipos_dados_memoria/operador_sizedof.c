b416a3a9d570eb2f9a2da80b7ca615b52d72b58f
Vinicius <marcusmvdiass@gmail.com>
2026-08-30 00:40:52 -0400

    Descobrir tamnhos de variaveis e tipos usando o sizedof

---
+++ exercicios/04_tipos_dados_memoria/operador_sizedof.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //Descobrir o tamanho em memoria de cada tipo de dado com o sizeof
+    //Para saber o tamanho do tipo usamos o getchar(tipo)
+    //Para saber o tamanho da variavel usamos o getchar x -> variavel
+    
+    int a;
+    char b;
+    float c;
+
+    printf("\n\tinteiro = %d\n", sizeof(int));
+    printf("\n\tcaractere = %d\n", sizeof(char));
+    printf("\n\tfloat = %d\n", sizeof(float));
+    printf("\n\tdouble = %d\n", sizeof(double));
+
+    printf("\n\tinteiro = %d\n", sizeof a);
+    printf("\n\tcaractere = %d\n", sizeof b);
+    printf("\n\tfloat = %d\n", sizeof c);
+
+
+}
