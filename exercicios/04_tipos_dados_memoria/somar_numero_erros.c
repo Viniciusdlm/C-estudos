e87e9ed2260577d2443a9176e42fc522d40edda4
Vinicius <marcusmvdiass@gmail.com>
2026-08-30 18:29:37 -0400

    Somando dois numeros inteiros e resultando em numeros negativos

---
+++ exercicios/04_tipos_dados_memoria/somar_numero_erros.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int a, b, c;
+    a = 2147483647;
+    b = 1;
+    c = a + b;
+
+    printf("\n\ta= %d\n\tb = %d\n\tc = %d\n\n\n", a,b,c);
+
+
+
+
+}
