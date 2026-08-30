eb193844a479e0ea36480f3f52abecd7aac45673
Vinicius <marcusmvdiass@gmail.com>
2026-08-30 18:53:14 -0400

    Lendo o exercicio de somar numeros com erro e usando no exemplo do long

--- exercicios/04_tipos_dados_memoria/aumentar_memoria_long.c
+++ exercicios/04_tipos_dados_memoria/aumentar_memoria_long.c
@@ -4,6 +4,13 @@
 int main(){
 
    long long int valor;
+   long long int a, b, c;
+
+    a = 2147483647;
+    b = 1;
+    c = a + b;
+
+    printf("\n\ta= %lld\n\tb = %lld\n\tc = %lld\n\n\n", a, b, c);
 
     printf("\n\tTamanho: %d\n", sizeof valor);
 
