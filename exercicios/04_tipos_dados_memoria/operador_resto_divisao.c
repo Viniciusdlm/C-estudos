7363fc32490a4a9fde53d9d530e0eb32fd7e9284
Vinicius <marcusmvdiass@gmail.com>
2026-09-02 22:34:38 -0400

    Usando o % para mostrar o resto da divisao

---
+++ exercicios/04_tipos_dados_memoria/operador_resto_divisao.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int v1, v2, v3;
+    v1 = 10;
+    v2 = 3;
+    v3 = v1%v2; // 
+    
+    printf("\n\tResto da divisao: %d\n", v3);
+
+
+
+}
