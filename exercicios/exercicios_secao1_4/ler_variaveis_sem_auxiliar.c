c23d99053ce47557db3b94f8b2a7d9f8baf83550
Vinicius <marcusmvdiass@gmail.com>
2026-09-06 22:40:04 -0400

    Exercicio para fazer o casting sem uma variavel auxiliar

---
+++ exercicios/exercicios_secao1_4/ler_variaveis_sem_auxiliar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int valor1, valor2;
+    printf("\n\tDigite dois numeros inteiros: ");
+    scanf("%d%d", &valor1, &valor2);
+
+    valor1= valor1+valor2;
+    valor2= valor1-valor2;
+    valor1= valor1 - valor2;
+
+    
+    
+
+printf("\n\tValor 1: %d\n\tValor 2: %d\n\t", valor1, valor2);
+}
