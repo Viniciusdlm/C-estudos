e240870e4df1d33f6e28dd54eaff7d527ffdc7c1
Vinicius <marcusmvdiass@gmail.com>
2026-09-07 14:28:06 -0400

    Exercicio de estrutura de decisao simples, verificando se um nmr é negativo

---
+++ exercicios/05_estrutura_de_decisao/estrutura_decisao_simples.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    if(-5 < 0 ){  //SE O NUMERO -5 FOR MENOR QUE 0 ENTAO ELE VAI SER 1 (RESULTADO POSITIVO) E VAI MOSTRAR A PROXIMA LINHA
+        printf("\n\tValor negativo!\n\n");
+    }
+
+
+}
