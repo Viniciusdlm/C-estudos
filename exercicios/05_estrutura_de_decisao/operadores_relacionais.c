59a82cec6a42015c83d942672c9054727cf6241f
Vinicius <marcusmvdiass@gmail.com>
2026-09-06 23:38:28 -0400

    Exercicio para entender os tipos de operadores relacionais

---
+++ exercicios/05_estrutura_de_decisao/operadores_relacionais.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    // Exercicio de operadores relacionais 0 ou 1 :  > , < , >= , <= , == , != .
+    printf("\n\t10 < 15 --> %d\n\t", 10 < 15);
+    printf("\n\t10 > 15 --> %d\n\t", 10 > 15);
+    printf("\n\t10 >= 15 -->%d\n\t", 10 >= 15);
+    printf("\n\t10 <= 15 --> %d\n\t", 10 <= 15);
+    printf("\n\t10 == 15 --> %d\n\t", 10 == 15);
+    printf("\n\t10 == 10 --> %d\n\t", 10 == 10);
+    printf("\n\t10 != 15 --> %d\n\t", 10 != 15);
+
+
+
+}
