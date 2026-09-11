0bc4c445d240881c6c1fb96d85fd19cc96c6f698
Vinicius <marcusmvdiass@gmail.com>
2026-09-10 21:51:44 -0400

    Corrigindo exercicio da secao 1

--- exercicios/exercicios_secao1_4/ler_varias_variaveis.c
+++ exercicios/exercicios_secao1_4/ler_varias_variaveis.c
@@ -3,11 +3,16 @@
 
 int main(){
 
-char sexo;
-int idade;
-float peso, altura;
+    char sexo;
+    int idade;
+    float peso;
+    float altura;
 
-printf("\tDigite seu sexo ( f ou m ), idade, peso e altura: ");
-scanf("%c%d%f%f", &sexo, &idade, &peso, &altura);
-printf("\n\tSeu sexo é: %c\n\tSua idade é: %d\n\tSeu peso é: %f\n\tSua altura é: %f\n\t", sexo, idade, peso, altura);
+    printf("\n\tDigite seu sexo ( f ou m ), idade, peso e altura: \n\t");
+    scanf("%c%d%f%f", &sexo, &idade, &peso, &altura);
+    
+
+    printf("\n\tSeu sexo é: %c\n\tSua idade é: %d\n\tSeu peso é: %f\n\tSua altura é: %f\n\t", sexo, idade, peso, altura);
+
+
 }
