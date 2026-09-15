d76239679057c948dd0d4aa693c2f3cf521556aa
Viniciusdlm <marcusmvdiass@gmail.com>
2026-09-14 21:30:14 -0400

    Exercicio de estrutura de decisao completa com if e else.

---
+++ exercicios/05_estrutura_de_decisao/estrutura_decisao_completa.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	//Aula de decisao completa.
+	if (5 < 0){
+		printf("\tValor Negativo!\n\n");
+	}
+	else{
+		printf("\tValor Positivo ou zero!\n\n");
+	}
+	
+	printf("\tApos o if\n\n");
+
+return 0;
+}
+
+
+
+
