893d58738a9fe4fc433b67c8d27e21069da9449c
Viniciusdlm <marcusmvdiass@gmail.com>
2026-09-14 21:38:13 -0400

    Exercicio de verdadeiro ou falso com !=

---
+++ exercicios/05_estrutura_de_decisao/verdadeiro_e_falso.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	/* Verdadeiro e falso na linguagem em C,
+	0 - falso
+	!= 0 verdadeiro */
+
+	if (1){
+		printf("vvvvvvvvvvvvvvvvvvvvvvv\n");
+	}
+	else{
+		printf("++++++++++++++++++++++\n");
+	}
+
+return 0;
+}
+
+
