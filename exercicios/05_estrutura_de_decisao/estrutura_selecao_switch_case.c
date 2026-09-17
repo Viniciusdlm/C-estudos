413e0c36cdb959b45b8f7f86ae99691ac6b339a1
Viniciusdlm <marcusmvdiass@gmail.com>
2026-09-16 20:19:59 -0400

    Exercicio de estrutura de selecao switch case

---
+++ exercicios/05_estrutura_de_decisao/estrutura_selecao_switch_case.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+//Aula de estrutura de selecao switch case
+
+int cod = 1;
+
+switch(cod){
+case 1:
+	printf("Janeiro\n");
+	break;
+case 2:
+	printf("Fevereiro\n");
+	break;
+case 3:
+	printf("Marco\n");
+	break;
+default:
+	printf("Valor inesperado\n");
+	break;
+}
+
+
+
+
+
+}
