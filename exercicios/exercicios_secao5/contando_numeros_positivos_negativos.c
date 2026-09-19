dab12761c8831f53f418fa1bc51fafda4ab57577
Viniciusdlm <marcusmvdiass@gmail.com>
2026-09-18 20:05:14 -0400

    Exercicio de contar numeros negativos e positivos usando o incremento

---
+++ exercicios/exercicios_secao5/contando_numeros_positivos_negativos.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int main(){
+
+int num, positivos, negativos, zeros;
+
+positivos = 0;
+negativos = 0;
+zeros = 0;
+
+printf}("Digite um valor: \n");
+scanf("%d", &num);
+
+if(num < 0)
+	negativos--;
+else if (num > 0)
+	positivos++;
+else
+	zeros++;
+
+printf}("Digite um valor: \n");
+scanf("%d", &num);
+
+if(num < 0)
+        negativos--;
+else if (num > 0)
+        positivos++;
+else
+        zeros++;
+
+printf}("Digite um valor: \n");
+scanf("%d", &num);
+
+if(num < 0)
+        negativos--;
+else if (num > 0)
+        positivos++;
+else
+        zeros++;
+
+printf}("Digite um valor: \n");
+scanf("%d", &num);
+
+if(num < 0)
+        negativos--;
+else if (num > 0)
+        positivos++;
+else
+        zeros++;
+
+
+printf}("Digite um valor: \n");
+scanf("%d", &num);
+
+if(num < 0)
+        negativos--;
+else if (num > 0)
+        positivos++;
+else
+        zeros++;
+
+printf("Positivos: %d\t\nNegativos: %d\t\nZeros: %d\t\n\n", positivos, negativos, zeros);
+
+
+}
